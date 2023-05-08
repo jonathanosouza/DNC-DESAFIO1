896a6f8d82f4dfe6d79aef634827433f73bd6c84
jonathanosouza <jonathanosouza@gmail.com>
2023-05-08 15:12:38 -0300

    Desafio 1 - DNC

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('myForm');
+const submitButton = document.getElementById('submitButton');
+
+form.addEventListener('submit', () => {
+   // impede o recarregamento da página
+    alert('Formulário enviado com sucesso!');
+  });
